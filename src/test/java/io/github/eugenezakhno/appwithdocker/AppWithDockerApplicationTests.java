1f4168a12fc82d0d73300b1c55b8b3274fc0a437
EugeneZakhno <zohan1234567@gmail.com>
2020-08-30 22:43:29 +0300

    init project

---
+++ src/test/java/io/github/eugenezakhno/appwithdocker/AppWithDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.eugenezakhno.appwithdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWithDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
